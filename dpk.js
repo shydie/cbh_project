db3eed81d24981a07070ec74ff4c27476d545135
Artur Skorodumov <artur.skorodumov@affirm.com>
2023-03-19 11:01:04 +0100

    refactoring

--- dpk.js
+++ dpk.js
@@ -1,28 +1,56 @@
 const crypto = require("crypto");
 
+// exports.deterministicPartitionKey = (event) => {
+//   const TRIVIAL_PARTITION_KEY = "0";
+//   const MAX_PARTITION_KEY_LENGTH = 256;
+//   let candidate;
+
+//   if (event) {
+//     if (event.partitionKey) {
+//       candidate = event.partitionKey;
+//     } else {
+//       const data = JSON.stringify(event);
+//       candidate = crypto.createHash("sha3-512").update(data).digest("hex");
+//     }
+//   }
+
+//   if (candidate) {
+//     if (typeof candidate !== "string") {
+//       candidate = JSON.stringify(candidate);
+//     }
+//   } else {
+//     candidate = TRIVIAL_PARTITION_KEY;
+//   }
+//   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
+//     candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+//   }
+//   return candidate;
+// };
+
+const createHashDigest = (data) => {
+  return crypto.createHash("sha3-512").update(data).digest("hex");
+}
+
+// My version is more readable as it contains less complex conditions and reassignings of variables
+// We cut all the edge cases in the start and then process the regular flow
+// Also removed repeated method calls
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+  // returns TRIVIAL_PARTITION_KEY if no event
+  if (!event) {
+    return TRIVIAL_PARTITION_KEY;
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+  // returns hashed stringified event if no partitionKey
+  if (!event.partitionKey) {
+    const data = JSON.stringify(event);
+    return createHashDigest(data)
   }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
-  return candidate;
+
+  const { partitionKey } = event;
+  const candidate = typeof partitionKey !== "string" ? JSON.stringify(partitionKey) : partitionKey;
+
+  return candidate.length > MAX_PARTITION_KEY_LENGTH ? createHashDigest(candidate) : candidate
 };
--- dpk.test.js
+++ dpk.test.js
@@ -1,8 +1,31 @@
 const { deterministicPartitionKey } = require("./dpk");
 
+const key = 'x'.repeat(256);
+const longKey = 'x'.repeat(300);
+
 describe("deterministicPartitionKey", () => {
   it("Returns the literal '0' when given no input", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
+  });
+
+  it("Returns the encoded value when given input event partitionKey length > 256", () => {
+    const trivialKey = deterministicPartitionKey({ partitionKey: longKey });
+    expect(trivialKey).toBe("523a0d2bd185ddcb7fb5e8d94f241da1cb620ed27ca715f9b0333ed38274fe2f47d35b7aed799cd1d35a68a728ceabb1f0c3ae6f6befbe94b4234e68aaa120da");
+  });
+
+  it("Returns the event.partitionKey itself when given input event.partitionKey length eq 256", () => {
+    const trivialKey = deterministicPartitionKey({ partitionKey: key });
+    expect(trivialKey).toBe(key);
+  });
+
+  it("Returns the encoded value when given input event has no partitionKey", () => {
+    const trivialKey = deterministicPartitionKey({});
+    expect(trivialKey).toBe("c1802e6b9670927ebfddb7f67b3824642237361f07db35526c42c555ffd2dbe74156c366e1550ef8c0508a6cc796409a7194a59bba4d300a6182b483d315a862");
+  });
+
+  it("Returns the stringified partitionKey value when given input event.partitionKey not a string", () => {
+    const trivialKey = deterministicPartitionKey({ partitionKey: { randomProp: '256' }});
+    expect(trivialKey).toBe("{\"randomProp\":\"256\"}");
   });
 });
